0116ddf1771f9ae2d938c1a9e75c32aaf39e705a
venkat-the-coder <venkatprabhu0113@gmail.com>
2025-06-30 13:48:16 +0530

    ef core applied

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using EntityframeWorkCore.Data;
+using EntityframeWorkCore.Models;
+
+
+using (var dbContext = new DbContextClass())
+{
+    var manager = new Manager
+    {
+        Name = "pari",
+    };
+
+    dbContext.Managers.Add(manager);
+
+
+    var employee = new Employee
+    {
+        Name = "kumar",
+        Manager = manager
+    };
+
+    dbContext.Employees.Add(employee);
+
+    dbContext.SaveChanges();
+
+    var responses = dbContext.Employees.ToList();
+
+    foreach (var employ in responses)
+    {
+        Console.WriteLine(employee.Name);
+    }
+
+    var response2 = dbContext.Employees.SingleOrDefault(c => c.EmployeeId == 4);
+    Console.WriteLine(employee.Name);
+    
+}
